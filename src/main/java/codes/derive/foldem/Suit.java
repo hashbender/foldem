dc0a377c78e369a3c5723ebe5609f4f204e2208e
ableiten <mail@derive.codes>
2017-03-19 06:25:42 +1300

    Add documenting comments

--- src/main/java/codes/derive/foldem/Suit.java
+++ src/main/java/codes/derive/foldem/Suit.java
@@ -4,11 +4,14 @@ package codes.derive.foldem;
  * Represents a suit for a card in a standard 52 card deck.
  */
 public enum Suit {
-	SPADES,
-	CLUBS,
-	HEARTS,
-	DIAMONDS;
+	SPADES, CLUBS, HEARTS, DIAMONDS;
 	
+	/**
+	 * Obtains the shorthand character for this suit. Equivalent to
+	 * <code>name().toLowercase().charAt(0)</code>
+	 * 
+	 * @return The shorthand character for this suit.
+	 */
 	public char getShorthand() {
 		return name().toLowerCase().charAt(0);
 	}
