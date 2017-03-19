cf106b520ca20f6ef8c922941dd94e3223fe77a0
ableiten <mail@derive.codes>
2017-03-20 08:37:50 +1300

    Refactored pretty-cards

--- src/main/java/codes/derive/foldem/examples/BasicCalc.java
+++ src/main/java/codes/derive/foldem/examples/BasicCalc.java
@@ -6,7 +6,7 @@ import codes.derive.foldem.EquityCalculator;
 import codes.derive.foldem.EquityCalculator.Equity;
 import codes.derive.foldem.Foldem;
 import codes.derive.foldem.hand.Hand;
-import codes.derive.foldem.util.NeatCards;
+import codes.derive.foldem.util.PrettyCards;
 
 public class BasicCalc {
 
@@ -14,7 +14,7 @@ public class BasicCalc {
 		EquityCalculator ec = new EquityCalculator();
 		Map<Hand, Equity> e = ec.calculate(Foldem.hand(Foldem.card("Qs"), Foldem.card("Qh")), Foldem.hand(Foldem.card("Js"), Foldem.card("Jh")));
 		for (Hand h : e.keySet()) {
-			System.out.println(NeatCards.represent(h) + " " + e.get(h));
+			System.out.println(PrettyCards.get(h) + " " + e.get(h));
 		}
 	}
 	
---
+++ src/main/java/codes/derive/foldem/util/PrettyCards.java
@@ -0,0 +1,77 @@
+package codes.derive.foldem.util;
+
+import codes.derive.foldem.Board;
+import codes.derive.foldem.Card;
+import codes.derive.foldem.Suit;
+import codes.derive.foldem.hand.Hand;
+
+/**
+ * Contains functions for pretty string formatting cards, similar to the Deuces
+ * Python evaluation library (https://github.com/worldveil/deuces).
+ */
+public class PrettyCards {
+
+	/**
+	 * Obtains the unicode character for the specified suit.
+	 * 
+	 * @param suit
+	 *            The suit.
+	 * @return The unicode character for the specified suit.
+	 */
+	public static char get(Suit suit) {
+		switch (suit) {
+		case SPADES:
+			return '\u2660';
+		case CLUBS:
+			return '\u2663';
+		case DIAMONDS:
+			return '\u2666';
+		case HEARTS:
+			return '\u2764';
+		}
+		throw new IllegalArgumentException("Invalid suit");
+	}
+
+	/**
+	 * Gets a pretty string representing the specified card.
+	 * 
+	 * @param card
+	 *            The card.
+	 * @return The pretty string for the specified card.
+	 */
+	public static String get(Card card) {
+		return new StringBuilder().append(Card.LABEL[card.getValue()])
+				.append(get(card.getSuit())).toString();
+	}
+
+	/**
+	 * Gets a pretty string representing the specified hand.
+	 * 
+	 * @param hand
+	 *            The hand.
+	 * @return The pretty string for the specified hand.
+	 */
+	public static String get(Hand hand) {
+		StringBuilder bldr = new StringBuilder();
+		for (Card card : hand.cards()) {
+			bldr.append(get(card)).append(',');
+		}
+		return bldr.substring(0, bldr.length() - 1).toString();
+	}
+
+	/**
+	 * Gets a pretty string representing the specified board.
+	 * 
+	 * @param board
+	 *            The board.
+	 * @return The pretty string for the specified board.
+	 */
+	public static String get(Board board) {
+		StringBuilder bldr = new StringBuilder();
+		for (Card card : board.cards()) {
+			bldr.append(get(card)).append(", ");
+		}
+		return bldr.substring(0, bldr.length() - 2).toString();
+	}
+
+}
