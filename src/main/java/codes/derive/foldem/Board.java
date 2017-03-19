b427ddac3771b2ba9ddc828bcee71dc70bdf6482
ableiten <mail@derive.codes>
2017-03-20 11:33:33 +1300

    Move enum to bottom

--- src/main/java/codes/derive/foldem/Board.java
+++ src/main/java/codes/derive/foldem/Board.java
@@ -19,53 +19,6 @@ public class Board {
 	private final Card[] cards = new Card[Constants.BOARD_SIZE];
 
 	/**
-	 * Represents a street on a given board.
-	 */
-	public enum Street {
-		
-		/**
-		 * Before the flop.
-		 */
-		PREFLOP(0),
-		
-		/**
-		 * On the flop, 3 cards.
-		 */
-		FLOP(3),
-		
-		/**
-		 * On the turn, 4 cards.
-		 */
-		TURN(4),
-		
-		/**
-		 * On the river, 5 cards.
-		 */
-		RIVER(5);
-		
-		/* The number of cards available on this street */
-		private final int cards;
-		
-		/**
-		 * Constructs a street with the provided number of cards.
-		 * @param cards
-		 * 		The number of cards available on the street.
-		 */
-		private Street(int cards) {
-			this.cards = cards;
-		}
-		
-		/**
-		 * Gets the number of cards available on the street.
-		 * @return
-		 * 		The number of cards available on the street.
-		 */
-		public int cardCount() {
-			return cards;
-		}
-	}
-
-	/**
 	 * Constructs a new board, popping its cards from the deck specified.
 	 * @param deck
 	 * 		The deck to pop cards from.
@@ -137,5 +90,52 @@ public class Board {
 		}
 		return bldr.toString();
 	}
-	
+
+	/**
+	 * Represents a street on a given board.
+	 */
+	public enum Street {
+		
+		/**
+		 * Before the flop.
+		 */
+		PREFLOP(0),
+		
+		/**
+		 * On the flop, 3 cards.
+		 */
+		FLOP(3),
+		
+		/**
+		 * On the turn, 4 cards.
+		 */
+		TURN(4),
+		
+		/**
+		 * On the river, 5 cards.
+		 */
+		RIVER(5);
+		
+		/* The number of cards available on this street */
+		private final int cards;
+		
+		/**
+		 * Constructs a street with the provided number of cards.
+		 * @param cards
+		 * 		The number of cards available on the street.
+		 */
+		private Street(int cards) {
+			this.cards = cards;
+		}
+		
+		/**
+		 * Gets the number of cards available on the street.
+		 * @return
+		 * 		The number of cards available on the street.
+		 */
+		public int cardCount() {
+			return cards;
+		}
+	}
+
 }
