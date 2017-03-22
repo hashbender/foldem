f77571ba0cdc1445ccf1d961ba70ea9437d71440
ableiten <mail@derive.codes>
2017-03-22 20:34:55 +1300

    Add documentation

--- src/main/java/codes/derive/foldem/util/RandomContext.java
+++ src/main/java/codes/derive/foldem/util/RandomContext.java
@@ -2,11 +2,21 @@ package codes.derive.foldem.util;
 
 import java.util.Random;
 
-// would rather just have a central set of static utilities for randomness
+/**
+ * Contains a singleton {@link java.util.Random} context used internally by
+ * Fold'em. The context is initialized using the default seeding and does not
+ * change.
+ */
 public class RandomContext {
 
+	/* The singleton Random instance. */
 	private static final Random instance = new Random();
 	
+	/**
+	 * Obtains the singleton {@link java.util.Random} context.
+	 * @return
+	 * 		The singleton {@link java.util.Random} context.
+	 */
 	public static Random get() {
 		return instance;
 	}
